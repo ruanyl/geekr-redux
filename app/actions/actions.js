a8c8be6de10d91287753670f37d9318a0226ae38
ruanyl <ruanyu1@gmail.com>
2015-08-17 21:39:05 +0300

    react with redux

---
+++ app/actions/actions.js
@@ -0,0 +1,40 @@
+export const GOTO_PAGE = 'GOTO_PAGE';
+export const NEXT_PAGE = 'NEXT_PAGE';
+export const PREV_PAGE = 'PREV_PAGE';
+export const REQUEST_PAGE = 'REQUEST_PAGE';
+export const RECEIVE_PAGE = 'RECEIVE_PAGE';
+
+export function gotoPage(pageNo) {
+  return {
+    type: GOTO_PAGE,
+    pageNo
+  };
+}
+
+export function nextPage() {
+  return {
+    type: NEXT_PAGE
+  };
+}
+
+export function prevPage() {
+  return {
+    type: PREV_PAGE
+  };
+}
+
+export function requestPage(pageNo) {
+  return dispatch => {
+    return fetch('http://localhost:8080/output.json')
+      .then(res => res.json())
+      .then(json => dispatch(receivePage(json, pageNo)));
+  };
+}
+
+function receivePage(json, pageNo) {
+  return {
+    type: RECEIVE_PAGE,
+    json,
+    pageNo
+  };
+}
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  context: __dirname + "/app",
+  entry: {
+    javascript: "./index.js",
+    html: "./index.html"
+  },
+
+  output: {
+    filename: "app.js",
+    path: __dirname + "/dist"
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ["babel-loader?optional[]=runtime&stage=0"]
+      },
+      {
+        test: /\.html$/,
+        loader: "file?name=[name].[ext]"
+      }
+    ]
+  },
+
+  devtool: 'source-map',
+
+  externals: {
+    //'react': 'React'
+  }
+};
