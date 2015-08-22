6db595f1a1c07e44396128e9449a9d9a8a4148a0
ruanyl <ruanyu1@gmail.com>
2015-08-22 18:03:21 +0300

    fixed link not work

--- app/components/Post.js
+++ app/components/Post.js
@@ -4,7 +4,7 @@ export default class Post extends Component {
   render() {
     return (
       <li>
-        <a href="{this.props.url}" target="_blank">{this.props.title}</a>
+        <a href={this.props.url} target="_blank">{this.props.title}</a>
         <span>{this.props.time}</span>
         <span>{this.props.tag}</span>
       </li>
