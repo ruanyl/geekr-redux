02588c2e038df5aa011157399de25ab6a99a999e
ruanyl <ruanyu1@gmail.com>
2015-08-25 23:19:58 +0300

    support tags with different bg color

--- app/components/Post.js
+++ app/components/Post.js
@@ -7,7 +7,12 @@ export default class Post extends Component {
         <a className="post-link" href={this.props.url} target="_blank">{this.props.title}</a>
         { this.props.author ? <span className="author">{this.props.author}</span> : null }
         <span className="time">{this.props.time}</span>
-        <span className="tag">{this.props.tag}</span>
+        {this.props.tags.map(function(tag, i) {
+          let style = {
+            backgroundColor: tag.color
+          };
+          return <span key={i} className="tag" style={style}>{tag.name}</span>;
+        })}
       </li>
     );
   }
