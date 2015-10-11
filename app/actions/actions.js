fba94a250d04bffc338111255b60f43a54a2ae10
ruanyl <ruanyu1@gmail.com>
2015-10-11 13:46:23 +0300

    remove useless code

--- app/actions/actions.js
+++ app/actions/actions.js
@@ -1,40 +1,28 @@
+import fetch from 'isomorphic-fetch';
+
 export const GOTO_PAGE = 'GOTO_PAGE';
-export const NEXT_PAGE = 'NEXT_PAGE';
-export const PREV_PAGE = 'PREV_PAGE';
-export const REQUEST_PAGE = 'REQUEST_PAGE';
 export const RECEIVE_PAGE = 'RECEIVE_PAGE';
 
-export function gotoPage(pageNo) {
+function _gotoPage(pageNo) {
   return {
     type: GOTO_PAGE,
     pageNo
   };
 }
 
-export function nextPage() {
-  return {
-    type: NEXT_PAGE
-  };
-}
-
-export function prevPage() {
-  return {
-    type: PREV_PAGE
-  };
-}
-
-export function requestPage(pageNo) {
-  return dispatch => {
-    return fetch(`./page${pageNo}.json`)
-      .then(res => res.json())
-      .then(json => dispatch(receivePage(json, pageNo)));
-  };
-}
-
-function receivePage(json, pageNo) {
+function receivePage(json) {
   return {
     type: RECEIVE_PAGE,
-    json,
-    pageNo
+    json
   };
 }
+
+export function gotoPage(pageNo) {
+  return dispatch => {
+    dispatch(_gotoPage(pageNo));
+    return fetch(`./page${pageNo}.json`)
+      .then(res => res.json())
+      .then(json => dispatch(receivePage(json)));
+  };
+}
+
