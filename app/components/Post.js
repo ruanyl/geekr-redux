3358f3d477b3110e064604d2477413996c885a7c
ruanyl <ruanyu1@gmail.com>
2015-10-19 21:18:50 +0300

    ui update

--- app/components/Post.js
+++ app/components/Post.js
@@ -1,11 +1,14 @@
+require('skeleton');
+require('../static/css/style.css');
+
 import React, { Component, PropTypes } from 'react';
 
 export default class Post extends Component {
   render() {
     return (
-      <li>
-        <a className="post-link" href={this.props.url} target="_blank">{this.props.title}</a>
-        { this.props.author ? <span className="author">{this.props.author}</span> : null }
+      <li className='post-list'>
+        <p className='post-title'><a className="post-link" href={this.props.url} target="_blank">{this.props.title}</a></p>
+        { this.props.author ? <span className="post-author">{this.props.author}</span> : null }
         <span className="time">{this.props.time}</span>
         {this.props.tags.map(function(tag, i) {
           let style = {
